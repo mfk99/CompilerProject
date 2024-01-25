ca78f4a8ab1b7bd3e5752a126402019b262827b6
Matti Kähkönen <matti.kahkonen99@gmail.com>
2024-01-25 10:53:29 +0200

    complete week 1

--- src/compiler/tokenizer.py
+++ src/compiler/tokenizer.py
@@ -6,6 +6,7 @@ TokentType = Literal["int_literal",
                      "identifier", 
                      "parentheses", 
                      "operator", 
+                     "comment",
                      "end"]
 
 @dataclass(frozen=True)
@@ -16,19 +17,24 @@ class Token:
 def tokenize(source_code: str) -> list[str]:
   whitespace_re = re.compile(r'\s+')
   integer_re = re.compile(r'[0-9]')
-  identifier_re = re.compile(r'[a-zA-Z0-9_]*')
-  operator_re = re.compile(r'[+-]')
+  identifier_re = re.compile(r'[a-zA-Z0-9_]+')
   paren_re = re.compile(r'[()]')
+  comment_re = re.compile(r'#.*\n|//')
+  #multiline_comment_re = re.compile(r'/\*.*?\*/')
+  #doesn't work for whatever reason
+  #TODO fix
+  operator_re = re.compile(r'==|=|!=|<=|>=|<|>|\+|-|\*|/')
 
   position = 0
   result: list[Token] = []
 
   while position < len(source_code):
+    #nuh-uh bad solution, make it easier to add more shit
     match = whitespace_re.match(source_code, position)
     if match is not None:
       position = match.end()
       continue
-
+      
     match = integer_re.match(source_code, position)
     if match is not None:
       result.append(Token(
@@ -47,15 +53,6 @@ def tokenize(source_code: str) -> list[str]:
       position = match.end()
       continue
 
-    match = operator_re.match(source_code, position)
-    if match is not None:
-      result.append(Token(
-        type='operator',
-        text=source_code[position:match.end()]
-      ))
-      position = match.end()
-      continue
-
     match = paren_re.match(source_code, position)
     if match is not None:
       result.append(Token(
@@ -65,8 +62,28 @@ def tokenize(source_code: str) -> list[str]:
       position = match.end()
       continue
 
+    match = comment_re.match(source_code, position)
+    if match is not None:
+      position = match.end()
+      continue
+    
+    '''
+    match = multiline_comment_re.match(source_code, position)
+    if match is not None:
+      position = match.end()
+      continue
+    '''
+
+    match = operator_re.match(source_code, position)
+    if match is not None:
+      result.append(Token(
+        type='operator',
+        text=source_code[position:match.end()]
+      ))
+      position = match.end()
+      continue
+    
     raise Exception(f'Tokenization failed near {source_code[position:(position + 10)]}...')
 
   # TODO cleanup
-  print(result)
   return result
--- tests/tokenizer_test.py
+++ tests/tokenizer_test.py
@@ -8,3 +8,24 @@ def test_tokenizer_basics():
         Token(type='identifier', text='if'), 
         Token(type='int_literal', text='3'), 
         Token(type='identifier', text='while')]
+
+def test_tokenizer_single_operator():
+    assert tokenize("==") == [
+        Token(type='operator', text='==')]
+
+def test_tokenizer_operators():
+    assert tokenize("= == <= >") == [
+        Token(type='operator', text='='), 
+        Token(type='operator', text='=='), 
+        Token(type='operator', text='<='), 
+        Token(type='operator', text='>')]
+    
+def test_tokenizer_comment():
+    assert tokenize("#if  3\nwhile") == [ 
+        Token(type='identifier', text='while')]
+
+''' 
+def test_tokenizer_multiline_comment():
+    assert tokenize("/* if  3\nwhile */ <") == [ 
+        Token(type='operator', text='<')]
+'''
